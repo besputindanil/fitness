67827d77141a2ce9b53146f1960d03edae97f633
besputindanil <50333644+besputindanil@users.noreply.github.com>
2020-05-22 01:05:13 +0400

    Добавляет скролл

--- build/js/main.js
+++ build/js/main.js
@@ -1,7 +1,36 @@
 "use strict";
 
+var SCROLL_DURATION = 500;
+
+var mainBlockScroll = document.querySelector(".main-block__button");
+var subscriptionBlock = document.querySelector("#subscription");
 var tabToggles = document.querySelectorAll(".tabs__toggle");
 var tabItems = document.querySelectorAll(".tabs__item");
+
+var animation = function (duration) {
+  var temp;
+  return function (element) {
+    cancelAnimationFrame(temp);
+    var start = performance.now();
+    var from = window.pageYOffset || document.documentElement.scrollTop;
+    var to = element.getBoundingClientRect().top;
+    requestAnimationFrame(function step(timestamp) {
+      var progress = (timestamp - start) / duration;
+      progress > 1 && (progress = 1);
+      window.scrollTo(0, (from + to * progress) | 0);
+      progress < 1 && (temp = requestAnimationFrame(step));
+    });
+  };
+};
+
+var scrollMenu = animation(SCROLL_DURATION);
+
+var onMainBlockScroll = function (evt) {
+  evt.preventDefault();
+  scrollMenu(subscriptionBlock);
+};
+
+mainBlockScroll.addEventListener("click", onMainBlockScroll);
 
 var selectTabItem = function (name) {
   for (var i = 0; i < tabItems.length; i++) {
